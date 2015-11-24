c5c3fbda4eec301fd8eb190266cdeb70908ada90
hansCC <hans.cc@ufl.edu>
2015-11-23 20:15:53 -0500

    Create source1.h

---
+++ source1.h
@@ -0,0 +1,85 @@
+#pragma once
+#ifndef source1_h
+#define source1_h
+
+#include <string>
+using namespace std;
+
+class Hero {
+private:
+	int health;
+	int attack;
+	string name;
+	string gender;
+	string weapon;
+	int potion;
+	int gold;
+public:
+	Hero() {
+		cout << "I'm a hero" << endl;
+		int health = 10;
+		int attack = 10;
+		string name = "Gyro";
+		string gender = "female";
+		string weapon = "cheese stick";
+		int potion = 0;
+		int gold = 3;
+	}
+
+	void setGender(string x) {
+		gender = x;
+	}
+	string getGender() {
+		return gender;
+	}
+	void setHealth(int x) {
+		health = x;
+	}
+	int getHealth() {
+		return health;
+	}
+	void setAttack(int x) {
+		attack = x;
+	}
+	int getAttack() {
+		return attack;
+	}
+	void setName(string x) {
+		name = x;
+	}
+	string getName() {
+		return name;
+	}
+	void setWeapon(string x) {
+		weapon = x;
+	}
+	string getWeapon() {
+		return weapon;
+	}
+	void setPotion(int x) {
+		potion = x;
+	}
+	int getPotion()
+	{
+		return potion;
+	}
+	void setGold(int x)
+	{
+		gold = x;
+	}
+	int getGold()
+	{
+		return gold;
+	}
+
+	void takeDamage(int x)
+	{
+		health -= x;
+	}
+
+
+
+};
+
+
+#endif
